71662601111beacecb721aa27586120bf6cc7557
anupal-cpu <anupalnath656@gmail.com>
2022-04-27 12:33:20 +0530

    added todo ading feature
    this commit includes todo ading feature. only chance in the main.js

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
 import './style.css';
 import './index.scss';
+
+// selectors
+
+const insertTodoBtn = document.querySelector('.add-btn');
+const insertInput = document.querySelector('.insert--input');
+const todoParent = document.querySelector('.todos');
+
+// functions
+
+const addTodo = function () {
+  todoParent.innerHTML += `<div
+    class="todo flex justify-between items-center border border-orange-500 px-1"
+  >
+    <ul class="todo-list">
+      <li>${insertInput.value}</li>
+    </ul>
+    <span class="actions flex gap-x-1">
+      <img
+        class="w-5 h-4 cursor-pointer"
+        src="./check.svg"
+        alt="completed"
+      />
+      <img
+        class="w-4 h-4 cursor-pointer"
+        src="./trash-can.svg"
+        alt="delete"
+      />
+    </span>
+  </div>`;
+};
+
+// eventlisteners
+
+insertTodoBtn.addEventListener('click', () => {
+  addTodo();
+});
