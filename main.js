3898e3e1a40e93f4684cc1af0d4084b125ef57ef
anupal-cpu <anupalnath656@gmail.com>
2022-05-03 18:52:06 +0530

    all the simple functionality of guess my number has been done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css';
 import './index.scss';
 import questionSvg from './assets.js';
+// import ifGuessSuccess from './guessSuccess.js';
+// import guessSuccess from './guessSuccess.js';
 
 /* qestion-mark-place
 guide-text
@@ -21,30 +23,58 @@ const currentScore = document.querySelector('.current--score-num ');
 const checkBtn = document.querySelector('.btn-check');
 const restartBtn = document.querySelector('.restart-btn');
 const randomNumber = document.querySelector('.random-number');
+const whatSvg = document.querySelector('.what-svg');
 
 // functions
 // ----------------
 
 // generate random number
 
-const setRandomNumber = function (elem) {
-  const randomElem = elem;
-  elem.textContent = Math.floor(Math.random() * 20);
+const setRandom = Math.floor(Math.random() * 21);
+randomNumber.textContent = setRandom;
+
+// this function will run if the guess is smaller than the random number
+
+const ifGuessSmall = function () {
+  guideText.textContent = 'Number is too small please enter a bigger number';
 };
 
-setRandomNumber(randomNumber);
+// this function will run if the guess is higher than the random number
+
+const ifGuessBig = function () {
+  guideText.textContent = 'Number is too big please enter a smaller number';
+};
+
+// if guess is true then this function will run
+
+const ifGuessSuccess = function () {
+  guideText.textContent = 'Hurray! you have gussed the number 😀';
+  if (currentValue > +highScore.textContent)
+    highScore.textContent = currentValue + 1;
+  whatSvg.style.opacity = 0;
+  randomNumber.style.opacity = 1;
+  guideText.style.color = 'green';
+  currentScore.textContent = +currentScore.textContent + 1;
+};
+
+console.log(setRandom);
 
 // compare number
 
+let currentValue = 20;
+
 const checkNumber = function () {
   const enteredNumber = checkInput.value;
-  const randomNum = +randomNumber.textContent;
-  if (enteredNumber < randomNum) {
-    guideText.textContent = 'Number is too small please enter a bigger number';
-  } else if (enteredNumber > randomNum) {
-    guideText.textContent = 'Number is too big please enter a smaller number';
-  } else if (enteredNumber == randomNum) {
-    guideText.textContent = 'Hurray! you have gussed the number 😀';
+
+  // decrement current score
+  currentValue -= 1;
+  currentScore.textContent = currentValue;
+  if (+enteredNumber < setRandom) {
+    ifGuessSmall();
+  } else if (+enteredNumber > setRandom) {
+    ifGuessBig();
+  } else if (+enteredNumber === setRandom) {
+    ifGuessSuccess();
   }
 };
 
